f4740c4615779b3293b2d8faa039fadc5e6933ba
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-05-30 15:35:55 +0200

    Initial commit

---
+++ src/test/java/com/github/jbence1994/webshop/WebshopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jbence1994.webshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
