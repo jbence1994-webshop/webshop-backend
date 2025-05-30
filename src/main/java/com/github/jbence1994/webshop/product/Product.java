b06b879623fccefbd64fc213c528e07f17e2ccba
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-05-30 17:05:16 +0200

    Define Product JPA entity

---
+++ src/main/java/com/github/jbence1994/webshop/product/Product.java
@@ -0,0 +1,32 @@
+package com.github.jbence1994.webshop.product;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private BigDecimal price;
+
+    private String unit;
+
+    private String description;
+}
