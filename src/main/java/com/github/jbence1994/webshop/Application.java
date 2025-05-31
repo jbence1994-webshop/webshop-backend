045be7481a0cfa77c2c53e61d23c52fc1e15567a
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-05-31 14:09:53 +0200

    Configure max image size, the allowed file extensions and the uploads directory path

--- src/main/java/com/github/jbence1994/webshop/Application.java
+++ src/main/java/com/github/jbence1994/webshop/Application.java
@@ -1,9 +1,12 @@
 package com.github.jbence1994.webshop;
 
+import com.github.jbence1994.webshop.photo.FileExtensionsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(value = {FileExtensionsConfig.class})
 public class Application {
 
     public static void main(String[] args) {
