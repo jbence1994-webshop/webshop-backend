c5f6743631b27128ab3de18251461b1fe109538d
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-05-31 12:18:20 +0200

    Introduce a primary key into the products_photos table

--- src/main/resources/db/migration/V1__database_schema.sql
+++ src/main/resources/db/migration/V1__database_schema.sql
@@ -6,7 +6,7 @@ USE webshop;
 
 CREATE TABLE IF NOT EXISTS products
 (
-    id          BIGINT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id          BIGINT         NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name        VARCHAR(255)   NOT NULL,
     price       DECIMAL(10, 2) NOT NULL,
     unit        VARCHAR(255)   NOT NULL,
@@ -15,6 +15,7 @@ CREATE TABLE IF NOT EXISTS products
 
 CREATE TABLE IF NOT EXISTS product_photos
 (
+    id         BIGINT      NOT NULL PRIMARY KEY AUTO_INCREMENT,
     product_id BIGINT      NOT NULL,
     file_name  VARCHAR(41) NOT NULL UNIQUE,
     CONSTRAINT fk_product_photos_products
