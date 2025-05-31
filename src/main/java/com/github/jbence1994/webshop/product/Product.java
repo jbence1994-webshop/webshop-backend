f470eb139866911637959a7b6124654b625f4e10
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-05-31 12:19:16 +0200

    Create ProductPhoto JPA entity

--- src/main/java/com/github/jbence1994/webshop/product/Product.java
+++ src/main/java/com/github/jbence1994/webshop/product/Product.java
@@ -1,9 +1,13 @@
 package com.github.jbence1994.webshop.product;
 
+import com.github.jbence1994.webshop.photo.ProductPhoto;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,6 +15,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "products")
@@ -31,4 +37,8 @@ public class Product {
     private String unit;
 
     private String description;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    @JoinColumn(name = "product_id")
+    private Set<ProductPhoto> photos = new HashSet<>();
 }
