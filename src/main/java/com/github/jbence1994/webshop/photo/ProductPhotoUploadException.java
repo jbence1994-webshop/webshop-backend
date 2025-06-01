6fcbef8ef0b3c725d293239b7b3afed28a24b581
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-06-02 00:32:40 +0200

    Fix the exception message

--- src/main/java/com/github/jbence1994/webshop/photo/ProductPhotoUploadException.java
+++ src/main/java/com/github/jbence1994/webshop/photo/ProductPhotoUploadException.java
@@ -2,6 +2,6 @@ package com.github.jbence1994.webshop.photo;
 
 public class ProductPhotoUploadException extends RuntimeException {
     public ProductPhotoUploadException() {
-        super("Uploading the photo was not successful.");
+        super("The photo could not be uploaded successfully.");
     }
 }
