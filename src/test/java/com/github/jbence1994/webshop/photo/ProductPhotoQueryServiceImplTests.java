ea1dc269d26e0a2c5f49563c82549a0a74b40f38
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-06-02 11:53:36 +0200

    Add public access modifiers to the test classes

--- src/test/java/com/github/jbence1994/webshop/photo/ProductPhotoQueryServiceImplTests.java
+++ src/test/java/com/github/jbence1994/webshop/photo/ProductPhotoQueryServiceImplTests.java
@@ -18,7 +18,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class ProductPhotoQueryServiceImplTests {
+public class ProductPhotoQueryServiceImplTests {
 
     @Mock
     private ProductService productService;
--- src/test/java/com/github/jbence1994/webshop/product/ProductServiceImplTests.java
+++ src/test/java/com/github/jbence1994/webshop/product/ProductServiceImplTests.java
@@ -23,7 +23,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class ProductServiceImplTests {
+public class ProductServiceImplTests {
 
     @Mock
     private ProductRepository productRepository;
