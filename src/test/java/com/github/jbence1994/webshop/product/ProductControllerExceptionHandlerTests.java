24e03f80ceabfbfc9229931eabb69c7cf9342316
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-06-02 19:06:15 +0200

    Add unit tests for ProductControllerExceptionHandler

---
+++ src/test/java/com/github/jbence1994/webshop/product/ProductControllerExceptionHandlerTests.java
@@ -0,0 +1,28 @@
+package com.github.jbence1994.webshop.product;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(MockitoExtension.class)
+class ProductControllerExceptionHandlerTest {
+
+    @InjectMocks
+    private ProductControllerExceptionHandler productControllerExceptionHandler;
+
+    @Test
+    void handleProductNotFoundExceptionTest() {
+        var exception = new ProductNotFoundException(1L);
+
+        var response = productControllerExceptionHandler.handleProductNotFoundException(exception);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals("No product was found with the given ID: #1.", response.getBody().error());
+    }
+}
