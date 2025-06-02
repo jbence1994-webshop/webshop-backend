a958a5d8609f5941fbb02e1d3624f93e0b1a3914
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2025-06-02 10:16:30 +0200

    Mapping fix (#11)

--- src/main/java/com/github/jbence1994/webshop/product/ProductMapper.java
+++ src/main/java/com/github/jbence1994/webshop/product/ProductMapper.java
@@ -1,10 +1,13 @@
 package com.github.jbence1994.webshop.product;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
     ProductDto toDto(Product product);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "photos", ignore = true)
     Product toEntity(CreateProductDto productDto);
 }
