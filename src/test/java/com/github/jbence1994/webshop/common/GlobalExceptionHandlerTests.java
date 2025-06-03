902bc3443236df61f7aae1d2f97afb7f82c62de0
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-06-03 20:51:47 +0200

    Add unit tests for GlobalExceptionHandler

--- src/test/java/com/github/jbence1994/webshop/common/GlobalExceptionHandlerTests.java
+++ src/test/java/com/github/jbence1994/webshop/common/GlobalExceptionHandlerTests.java
@@ -7,10 +7,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.multipart.support.MissingServletRequestPartException;
 
 import java.util.Collections;
+import java.util.List;
 
+import static com.github.jbence1994.webshop.common.FieldErrorTestObject.fieldError;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.mock;
@@ -45,5 +49,20 @@ public class GlobalExceptionHandlerTests {
         assertEquals(HttpStatus.BAD_REQUEST, result.getStatusCode());
         assertNotNull(result.getBody());
         assertEquals("The file must not be empty.", result.getBody().error());
+    }
+
+    @Test
+    public void handleValidationErrorsTest() {
+        var bindingResult = mock(BindingResult.class);
+        var exception = mock(MethodArgumentNotValidException.class);
+
+        when(bindingResult.getFieldErrors()).thenReturn(List.of(fieldError()));
+        when(exception.getBindingResult()).thenReturn(bindingResult);
+
+        var result = globalExceptionHandler.handleValidationErrors(exception);
+
+        assertEquals(HttpStatus.BAD_REQUEST, result.getStatusCode());
+        assertNotNull(result.getBody());
+        assertEquals(1, result.getBody().size());
     }
 }
