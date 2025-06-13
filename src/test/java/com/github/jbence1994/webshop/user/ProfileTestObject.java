d4b2579e2707248579deb4575e80dc7502a742fb
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-06-13 21:05:19 +0200

    Add unit tests for testing membership tier calculation

--- src/test/java/com/github/jbence1994/webshop/user/ProfileTestObject.java
+++ src/test/java/com/github/jbence1994/webshop/user/ProfileTestObject.java
@@ -10,7 +10,55 @@ import static com.github.jbence1994.webshop.user.ProfileTestConstants.MIDDLE_NAM
 import static com.github.jbence1994.webshop.user.ProfileTestConstants.PHONE_NUMBER;
 
 public final class ProfileTestObject {
-    public static Profile profile() {
+    public static Profile bronzeProfile1() {
+        return buildProfile(0);
+    }
+
+    public static Profile bronzeProfile2() {
+        return buildProfile(2_500);
+    }
+
+    public static Profile bronzeProfile3() {
+        return buildProfile(4_999);
+    }
+
+    public static Profile silverProfile1() {
+        return buildProfile(5_000);
+    }
+
+    public static Profile silverProfile2() {
+        return buildProfile(7_500);
+    }
+
+    public static Profile silverProfile3() {
+        return buildProfile(9_999);
+    }
+
+    public static Profile goldProfile1() {
+        return buildProfile(10_000);
+    }
+
+    public static Profile goldProfile2() {
+        return buildProfile(15_000);
+    }
+
+    public static Profile goldProfile3() {
+        return buildProfile(19_999);
+    }
+
+    public static Profile platinumProfile1() {
+        return buildProfile(20_000);
+    }
+
+    public static Profile platinumProfile2() {
+        return buildProfile(510_000);
+    }
+
+    public static Profile platinumProfile3() {
+        return buildProfile(1_000_000);
+    }
+
+    private static Profile buildProfile(int loyaltyPoints) {
         return new Profile(
                 1L,
                 null,
@@ -19,7 +67,7 @@ public final class ProfileTestObject {
                 LAST_NAME,
                 DATE_OF_BIRTH,
                 PHONE_NUMBER,
-                0,
+                loyaltyPoints,
                 LocalDateTime.now(),
                 LocalDateTime.now(),
                 address()
--- src/test/java/com/github/jbence1994/webshop/user/UserTestObject.java
+++ src/test/java/com/github/jbence1994/webshop/user/UserTestObject.java
@@ -2,14 +2,14 @@ package com.github.jbence1994.webshop.user;
 
 import java.time.LocalDateTime;
 
-import static com.github.jbence1994.webshop.user.ProfileTestObject.profile;
+import static com.github.jbence1994.webshop.user.ProfileTestObject.bronzeProfile1;
 import static com.github.jbence1994.webshop.user.UserTestConstants.EMAIL;
 import static com.github.jbence1994.webshop.user.UserTestConstants.HASHED_PASSWORD;
 import static com.github.jbence1994.webshop.user.UserTestConstants.PASSWORD;
 
 public final class UserTestObject {
     public static User user() {
-        return buildUser(1L, HASHED_PASSWORD, profile());
+        return buildUser(1L, HASHED_PASSWORD, bronzeProfile1());
     }
 
     public static User userAfterMappingFromDto() {
