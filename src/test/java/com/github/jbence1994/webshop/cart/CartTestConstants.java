7cd601f7756e201f69cf03c47642aa9bea931667
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-06-15 21:38:36 +0200

    Change 'created_at' column in 'carts' to DATETIME

--- src/test/java/com/github/jbence1994/webshop/cart/CartTestConstants.java
+++ src/test/java/com/github/jbence1994/webshop/cart/CartTestConstants.java
@@ -1,9 +1,9 @@
 package com.github.jbence1994.webshop.cart;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public interface CartTestConstants {
     UUID CART_ID = UUID.fromString("00492884-e657-4c6a-abaa-aef8f4240a69");
-    LocalDate CART_CREATED_AT = LocalDate.of(2025, 6, 9);
+    LocalDateTime CART_CREATED_AT = LocalDateTime.of(2025, 6, 9, 0, 0, 0);
 }
