fb0a4c1550f9739931e5796073d103ab38563e49
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-06-16 12:18:43 +0200

    Updating JPA entities according to the changes in db schema

--- src/main/java/com/github/jbence1994/webshop/coupon/Coupon.java
+++ src/main/java/com/github/jbence1994/webshop/coupon/Coupon.java
@@ -1,7 +1,12 @@
 package com.github.jbence1994.webshop.coupon;
 
+import com.github.jbence1994.webshop.user.User;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,6 +15,8 @@ import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "coupons")
@@ -25,6 +32,14 @@ public class Coupon {
     private BigDecimal amount;
 
     private LocalDateTime expirationDate;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_coupons",
+            joinColumns = @JoinColumn(name = "coupon_code", referencedColumnName = "code"),
+            inverseJoinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id")
+    )
+    private Set<User> users = new HashSet<>();
 
     public boolean isExpired() {
         return expirationDate.isBefore(LocalDateTime.now());
--- src/test/java/com/github/jbence1994/webshop/coupon/CouponTestObject.java
+++ src/test/java/com/github/jbence1994/webshop/coupon/CouponTestObject.java
@@ -1,18 +1,22 @@
 package com.github.jbence1994.webshop.coupon;
 
 import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
 
 import static com.github.jbence1994.webshop.coupon.CouponTestConstants.CODE_1;
 import static com.github.jbence1994.webshop.coupon.CouponTestConstants.CODE_2;
 import static com.github.jbence1994.webshop.coupon.CouponTestConstants.EXPIRED_COUPON_EXPIRATION_DATE;
 import static com.github.jbence1994.webshop.coupon.CouponTestConstants.NOT_EXPIRED_COUPON_EXPIRATION_DATE;
+import static com.github.jbence1994.webshop.user.UserTestObject.user;
 
 public final class CouponTestObject {
     public static Coupon notExpiredCoupon() {
         return new Coupon(
                 CODE_1,
                 BigDecimal.valueOf(5.00),
-                NOT_EXPIRED_COUPON_EXPIRATION_DATE
+                NOT_EXPIRED_COUPON_EXPIRATION_DATE,
+                Set.of(user())
         );
     }
 
@@ -20,7 +24,8 @@ public final class CouponTestObject {
         return new Coupon(
                 CODE_2,
                 BigDecimal.valueOf(10.00),
-                EXPIRED_COUPON_EXPIRATION_DATE
+                EXPIRED_COUPON_EXPIRATION_DATE,
+                new HashSet<>()
         );
     }
 }
