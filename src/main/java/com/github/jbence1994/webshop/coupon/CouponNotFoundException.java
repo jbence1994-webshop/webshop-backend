a402add550b275b09e41ab1cb2091fb60529443c
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-06-17 20:22:04 +0200

    Fix typo

--- src/main/java/com/github/jbence1994/webshop/coupon/CouponNotFoundException.java
+++ src/main/java/com/github/jbence1994/webshop/coupon/CouponNotFoundException.java
@@ -2,6 +2,6 @@ package com.github.jbence1994.webshop.coupon;
 
 public class CouponNotFoundException extends RuntimeException {
     public CouponNotFoundException(String code) {
-        super(String.format("No coupon was found with the give coupon code: '%s'", code));
+        super(String.format("No coupon was found with the given coupon code: '%s'", code));
     }
 }
