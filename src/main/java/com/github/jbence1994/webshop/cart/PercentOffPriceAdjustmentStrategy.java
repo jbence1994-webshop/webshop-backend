467e5686f069cea8cedfdda8cc650a15a2b73d26
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-06-20 18:24:51 +0200

    Add empty lines

--- src/main/java/com/github/jbence1994/webshop/cart/PercentOffPriceAdjustmentStrategy.java
+++ src/main/java/com/github/jbence1994/webshop/cart/PercentOffPriceAdjustmentStrategy.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class PercentOffPriceAdjustmentStrategy implements PriceAdjustmentStrategy {
+
     @Override
     public BigDecimal adjustPrice(BigDecimal totalPrice, BigDecimal value) {
         var discountValue = totalPrice
