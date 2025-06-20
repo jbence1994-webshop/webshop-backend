b922154447156200b850767677ac4f899d602378
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-06-20 19:22:20 +0200

    Add empty lines

--- src/main/java/com/github/jbence1994/webshop/cart/FixedAmountPriceAdjustmentStrategy.java
+++ src/main/java/com/github/jbence1994/webshop/cart/FixedAmountPriceAdjustmentStrategy.java
@@ -3,6 +3,7 @@ package com.github.jbence1994.webshop.cart;
 import java.math.BigDecimal;
 
 public class FixedAmountPriceAdjustmentStrategy implements PriceAdjustmentStrategy {
+
     @Override
     public BigDecimal adjustPrice(BigDecimal totalPrice, BigDecimal value) {
         return totalPrice.subtract(value).max(BigDecimal.ZERO);
