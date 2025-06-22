d0a2eec3829dbe591c06617a8bb8587d04b0bb42
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-06-22 14:26:48 +0200

    Remove NoArgsConstructor and Setter annotations

--- src/main/java/com/github/jbence1994/webshop/cart/AddItemToCartRequest.java
+++ src/main/java/com/github/jbence1994/webshop/cart/AddItemToCartRequest.java
@@ -3,13 +3,9 @@ package com.github.jbence1994.webshop.cart;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @Getter
-@Setter
 public class AddItemToCartRequest {
 
     @NotNull(message = "Product ID must be provided.")
--- src/main/java/com/github/jbence1994/webshop/cart/ApplyCouponToCartRequest.java
+++ src/main/java/com/github/jbence1994/webshop/cart/ApplyCouponToCartRequest.java
@@ -4,13 +4,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @Getter
-@Setter
 public class ApplyCouponToCartRequest {
 
     @NotNull(message = "Coupon code must be provided.")
