3260d963769ad57c893f37039b8683599a70b382
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-06-22 13:46:22 +0200

    Transform CartDto, CartItemDto and CartProductDto to record

--- src/main/java/com/github/jbence1994/webshop/cart/CartDto.java
+++ src/main/java/com/github/jbence1994/webshop/cart/CartDto.java
@@ -1,17 +1,13 @@
 package com.github.jbence1994.webshop.cart;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
-@AllArgsConstructor
-@Getter
-public class CartDto {
-    private UUID id;
-    private List<CartItemDto> items;
-    private String appliedCoupon;
-    private BigDecimal totalPrice;
+public record CartDto(
+        UUID id,
+        List<CartItemDto> items,
+        String appliedCoupon,
+        BigDecimal totalPrice
+) {
 }
