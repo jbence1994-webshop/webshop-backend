9521ad59c23692f4d420dfd318cf625c493852ce
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-06-24 18:52:23 +0200

    Convert all order related DTOs to record types

--- src/main/java/com/github/jbence1994/webshop/order/OrderDto.java
+++ src/main/java/com/github/jbence1994/webshop/order/OrderDto.java
@@ -1,24 +1,16 @@
 package com.github.jbence1994.webshop.order;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-public class OrderDto {
-    private Long id;
-    private BigDecimal totalPrice;
-    private BigDecimal discountAmount;
-    private BigDecimal shippingCost;
-    private String status;
-    private LocalDateTime createdAt;
-    private List<OrderItemDto> items;
+public record OrderDto(
+        Long id,
+        BigDecimal totalPrice,
+        BigDecimal discountAmount,
+        BigDecimal shippingCost,
+        String status,
+        LocalDateTime createdAt,
+        List<OrderItemDto> items
+) {
 }
--- src/main/java/com/github/jbence1994/webshop/order/OrderProductDto.java
+++ src/main/java/com/github/jbence1994/webshop/order/OrderProductDto.java
@@ -1,18 +1,10 @@
 package com.github.jbence1994.webshop.order;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.math.BigDecimal;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-public class OrderProductDto {
-    private Long id;
-    private String name;
-    private BigDecimal price;
+public record OrderProductDto(
+        Long id,
+        String name,
+        BigDecimal price
+) {
 }
