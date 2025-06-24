b4998b44ebab81d70b53071afd0728c545e00b3a
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-06-24 20:22:42 +0200

    Transform AddressDto, ProfileDto and UserDto to record

--- src/main/java/com/github/jbence1994/webshop/user/AddressDto.java
+++ src/main/java/com/github/jbence1994/webshop/user/AddressDto.java
@@ -1,18 +1,10 @@
 package com.github.jbence1994.webshop.user;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-public class AddressDto {
-    private String addressLine;
-    private String municipality;
-    private String province;
-    private String postalCode;
-    private String country;
+public record AddressDto(
+        String addressLine,
+        String municipality,
+        String province,
+        String postalCode,
+        String country
+) {
 }
--- src/main/java/com/github/jbence1994/webshop/user/ProfileDto.java
+++ src/main/java/com/github/jbence1994/webshop/user/ProfileDto.java
@@ -1,22 +1,14 @@
 package com.github.jbence1994.webshop.user;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.time.LocalDate;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-public class ProfileDto {
-    private String firstName;
-    private String middleName;
-    private String lastName;
-    private LocalDate dateOfBirth;
-    private String phoneNumber;
-    private int loyaltyPoints;
-    private AddressDto address;
+public record ProfileDto(
+        String firstName,
+        String middleName,
+        String lastName,
+        LocalDate dateOfBirth,
+        String phoneNumber,
+        int loyaltyPoints,
+        AddressDto address
+) {
 }
