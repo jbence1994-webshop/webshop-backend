f4ee808a7c14263cda83240b56a1e516c9dc4804
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-06-24 18:36:02 +0200

    Transform CouponDto to record

--- src/main/java/com/github/jbence1994/webshop/coupon/CouponDto.java
+++ src/main/java/com/github/jbence1994/webshop/coupon/CouponDto.java
@@ -1,21 +1,13 @@
 package com.github.jbence1994.webshop.coupon;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-public class CouponDto {
-    private String code;
-    private String type;
-    private BigDecimal value;
-    private String description;
-    private LocalDateTime expirationDate;
+public record CouponDto(
+        String code,
+        String type,
+        BigDecimal value,
+        String description,
+        LocalDateTime expirationDate
+) {
 }
