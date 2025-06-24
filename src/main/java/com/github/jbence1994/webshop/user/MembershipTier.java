05d3646a6510a5fac38cc46e49cc1af20b6c18b8
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-06-24 17:01:08 +0200

    Store min and max points for each membership tier

--- src/main/java/com/github/jbence1994/webshop/user/MembershipTier.java
+++ src/main/java/com/github/jbence1994/webshop/user/MembershipTier.java
@@ -6,16 +6,17 @@ import java.util.stream.Stream;
 
 @AllArgsConstructor
 public enum MembershipTier {
-    BRONZE(4_999),
-    SILVER(9_999),
-    GOLD(19_999),
-    PLATINUM(Integer.MAX_VALUE);
+    BRONZE(0, 4_999),
+    SILVER(5_000, 9_999),
+    GOLD(10_000, 19_999),
+    PLATINUM(20_000, Integer.MAX_VALUE);
 
-    private final int maxPoints;
+    private final int minLoyaltyPoints;
+    private final int maxLoyaltyPoints;
 
     public static MembershipTier fromPoints(int points) {
         return Stream.of(values())
-                .filter(tier -> points >= 0 && points <= tier.maxPoints)
+                .filter(tier -> points >= tier.minLoyaltyPoints && points <= tier.maxLoyaltyPoints)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(String.format("Invalid value: %d.", points)));
     }
