90d4ffbaa9b1a66e1b26d90a9a9289e458c9d78b
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-06-25 14:15:25 +0200

    Update TODO

--- src/main/java/com/github/jbence1994/webshop/checkout/CheckoutServiceImpl.java
+++ src/main/java/com/github/jbence1994/webshop/checkout/CheckoutServiceImpl.java
@@ -58,7 +58,7 @@ public class CheckoutServiceImpl implements CheckoutService {
         user.earnRewardPoints(earnedRewardPoints);
         order.setEarnedRewardPoints(earnedRewardPoints);
 
-        // TODO: Payment integration.
+        // TODO: Payment integration: order.getPayableTotalPrice() + order.getShippingCost() will be passed to Payment Gateway.
 
         // 1) If payment was successful:
         order.setStatus(OrderStatus.COMPLETED);
