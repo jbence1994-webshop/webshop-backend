31966a905d1e73b8ae86483bf447fdace548bafc
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-08-13 13:30:34 +0200

    Fix wrong relationship

--- src/main/java/com/github/jbence1994/webshop/product/Category.java
+++ src/main/java/com/github/jbence1994/webshop/product/Category.java
@@ -5,12 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @Table(name = "categories")
@@ -26,6 +28,6 @@ public class Category {
 
     private String name;
 
-    @OneToOne(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Product product;
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Product> product;
 }
