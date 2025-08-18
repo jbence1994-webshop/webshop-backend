f9ce09c930c4e06e4863303a4941326726da7c3e
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-08-18 17:21:27 +0200

    Enable all endpoints without authentication and authorization

---
+++ src/main/java/com/github/jbence1994/webshop/auth/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.github.jbence1994.webshop.auth;
+
+import lombok.SneakyThrows;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    @SneakyThrows
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        http
+                .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(registry -> registry.anyRequest().permitAll());
+
+        return http.build();
+    }
+}
