1c4b5893e85e6c5b941499f4fc01f01f024d954d
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-08-20 13:32:01 +0200

    Remove unnecessary setters from requests

--- src/main/java/com/github/jbence1994/webshop/user/ChangePasswordRequest.java
+++ src/main/java/com/github/jbence1994/webshop/user/ChangePasswordRequest.java
@@ -6,12 +6,10 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-@Setter
 @ConfirmNewPassword
 public class ChangePasswordRequest {
 
