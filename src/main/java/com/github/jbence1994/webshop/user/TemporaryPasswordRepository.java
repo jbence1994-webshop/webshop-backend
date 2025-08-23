e42a707eb9a8520537462002601e6bfa7e29c477
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-08-23 11:51:25 +0200

    Clean up obsolete temporary password in all cases

--- src/main/java/com/github/jbence1994/webshop/user/TemporaryPasswordRepository.java
+++ src/main/java/com/github/jbence1994/webshop/user/TemporaryPasswordRepository.java
@@ -2,8 +2,11 @@ package com.github.jbence1994.webshop.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TemporaryPasswordRepository extends JpaRepository<TemporaryPassword, Long> {
-    Optional<TemporaryPassword> findByPassword(String password);
+    List<TemporaryPassword> findAllByUserId(Long userId);
+
+    Optional<TemporaryPassword> findTopByUserIdOrderByExpirationDateDesc(Long userId);
 }
