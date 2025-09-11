28d2b65ee3a1d2f35a8456f7eb7b0eafabbdf814
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-09-11 14:52:40 +0200

    Revert "Add TODOs"
    This reverts commit b29a36d4aa5c133bed9c02e5aece01b9e9fb3173.

--- src/main/java/com/github/jbence1994/webshop/cart/Cart.java
+++ src/main/java/com/github/jbence1994/webshop/cart/Cart.java
@@ -45,7 +45,6 @@ public class Cart {
     @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<CartItem> items = new ArrayList<>();
 
-    // TODO: Remove this field.
     @ManyToOne
     @JoinColumn(name = "applied_coupon")
     private Coupon appliedCoupon;
