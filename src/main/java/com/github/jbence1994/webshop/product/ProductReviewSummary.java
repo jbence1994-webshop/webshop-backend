165614d93330c9f2b6c186f9c3a8ba79dd5d5fa6
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-10-03 14:55:24 +0200

    Define ProductReviewSummary entity

---
+++ src/main/java/com/github/jbence1994/webshop/product/ProductReviewSummary.java
@@ -0,0 +1,36 @@
+package com.github.jbence1994.webshop.product;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "product_review_summaries")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProductReviewSummary {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private String text;
+
+    private LocalDateTime expirationDate;
+}
