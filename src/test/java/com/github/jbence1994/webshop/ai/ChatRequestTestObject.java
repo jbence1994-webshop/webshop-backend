9cd20ad0797f065784ae9743e8ab64f7d8162053
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-10-04 12:57:24 +0200

    Temp. clean up 'conversationId' field in ChatRequest

--- src/test/java/com/github/jbence1994/webshop/ai/ChatRequestTestObject.java
+++ src/test/java/com/github/jbence1994/webshop/ai/ChatRequestTestObject.java
@@ -1,9 +1,7 @@
 package com.github.jbence1994.webshop.ai;
 
-import java.util.UUID;
-
 public final class ChatRequestTestObject {
     public static ChatRequest chatRequest() {
-        return new ChatRequest("Greet.", UUID.randomUUID());
+        return new ChatRequest("Greet.");
     }
 }
