02908a52c5d18cb933785f8174c5981aa90927f5
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-10-10 13:16:20 +0200

    Remove unused import

--- src/test/java/com/github/jbence1994/webshop/cart/CartServiceImplTests.java
+++ src/test/java/com/github/jbence1994/webshop/cart/CartServiceImplTests.java
@@ -21,7 +21,6 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
