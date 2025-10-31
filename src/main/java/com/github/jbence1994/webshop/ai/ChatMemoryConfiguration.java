3c120bbb88b961d44e2cfa9cecc27a023576cfb4
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2025-10-31 08:49:36 +0100

    Move 'maxMessages' parameter to environment variable

--- src/main/java/com/github/jbence1994/webshop/ai/ChatMemoryConfiguration.java
+++ src/main/java/com/github/jbence1994/webshop/ai/ChatMemoryConfiguration.java
@@ -1,5 +1,6 @@
 package com.github.jbence1994.webshop.ai;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.memory.ChatMemoryRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,14 +8,15 @@ import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 
 @Configuration
+@RequiredArgsConstructor
 public class ChatMemoryConfiguration {
+    private final ChatMemoryMaxMessagesConfig chatMemoryMaxMessagesConfig;
 
     @Bean
     public ChatMemory chatMemory(ChatMemoryRepository chatMemoryRepository) {
         return MessageWindowChatMemory.builder()
                 .chatMemoryRepository(chatMemoryRepository)
-                // FIXME: Move to env. variable
-                .maxMessages(20)
+                .maxMessages(chatMemoryMaxMessagesConfig.maxMessages())
                 .build();
     }
 }
