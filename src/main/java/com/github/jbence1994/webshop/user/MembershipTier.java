36c969ebd6018975bc42ad2d6fd18019244cfc09
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2025-11-07 12:32:10 +0100

    Define MembershipTier enum

---
+++ src/main/java/com/github/jbence1994/webshop/user/MembershipTier.java
@@ -0,0 +1,25 @@
+package com.github.jbence1994.webshop.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.stream.Stream;
+
+@RequiredArgsConstructor
+@Getter
+public enum MembershipTier {
+    BRONZE(0, 4_999),
+    SILVER(5_000, 9_999),
+    GOLD(10_000, 19_999),
+    PLATINUM(20_000, Integer.MAX_VALUE);
+
+    private final int minPoints;
+    private final int maxPoints;
+
+    public static MembershipTier fromPoints(int value) {
+        return Stream.of(values())
+                .filter(tier -> value >= tier.minPoints && value <= tier.maxPoints)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(String.format("Invalid value: %d.", value)));
+    }
+}
