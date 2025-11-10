ddf7c756b1c981727ec73ad496c35ffd78e14c26
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2025-11-10 19:45:55 +0100

    Moving coupons to Profile entity instead of User (#251)

--- src/main/java/com/github/jbence1994/webshop/user/User.java
+++ src/main/java/com/github/jbence1994/webshop/user/User.java
@@ -1,17 +1,14 @@
 package com.github.jbence1994.webshop.user;
 
-import com.github.jbence1994.webshop.coupon.Coupon;
 import com.github.jbence1994.webshop.product.Product;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -21,8 +18,6 @@ import lombok.Setter;
 import org.hibernate.annotations.GeneratedColumn;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @Entity
@@ -54,9 +49,6 @@ public class User {
 
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private Profile profile;
-
-    @ManyToMany(mappedBy = "users", fetch = FetchType.EAGER)
-    private List<Coupon> coupons = new ArrayList<>();
 
     public String getFirstName() {
         return profile.getFirstName();
