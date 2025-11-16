f0ec9a979b493e5106a1a49b139d8410dc1ac0bc
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2025-11-16 22:56:30 +0100

    Fix mapping

--- src/main/java/com/github/jbence1994/webshop/auth/UserIdentityMapper.java
+++ src/main/java/com/github/jbence1994/webshop/auth/UserIdentityMapper.java
@@ -2,13 +2,9 @@ package com.github.jbence1994.webshop.auth;
 
 import com.github.jbence1994.webshop.user.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserIdentityMapper {
 
-    @Mapping(target = "firstName", source = "profile.firstName")
-    @Mapping(target = "middleName", source = "profile.middleName")
-    @Mapping(target = "lastName", source = "profile.lastName")
     UserIdentity toUserIdentity(User user);
 }
